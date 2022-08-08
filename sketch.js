7d244e53c743bc7bcf0fca68a3866893355fe5cc
zestyneve <107005267+zestyneve@users.noreply.github.com>
2022-08-08 18:57:03 -0300

    jogo do dinossauro

--- sketch.js
+++ sketch.js
@@ -9,6 +9,15 @@ const PLAY =1
 const END =0
 var gamestate= PLAY
 var score=0
+var gameover,gameoverimg
+var reset,resetimg
+var trexcollide
+var pulo
+var morte
+var chekponto
+
+
+
 function preload(){
   trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
   groundImage = loadImage("ground2.png")
@@ -19,6 +28,12 @@ function preload(){
   obstacle4 = loadImage("obstacle4.png");
   obstacle5 = loadImage("obstacle5.png");
   obstacle6 = loadImage("obstacle6.png");
+  trexcollide= loadAnimation("trex_collided.png")
+  gameoverimg= loadImage("gameover.png")
+  pulo= loadSound("jump.mp3")
+  morte=loadSound("die.mp3")
+  chekponto=loadSound("checkpoint.mp3")
+  resetimg= loadImage("restart.png")
 }
 
 function setup(){
@@ -29,6 +44,10 @@ function setup(){
   //criando o trex
   trex = createSprite(50,160,20,50);
   trex.addAnimation("running", trex_running);
+  trex.addAnimation("collide",trexcollide)
+  trex.debug=false
+  //trex.setCollider("rectangle",0,0,400,50)
+  trex.setCollider("circle",0,0,30)
   edges = createEdgeSprites();
   
   //adicione dimensão e posição ao trex
@@ -38,36 +57,61 @@ function setup(){
   grouchao.addImage(groundImage )
   cactos=new Group()
 nuvens=new Group()
+gameover=createSprite(300,80,100,10)
+gameover.addImage(gameoverimg)
+gameover.scale=0.5
+gameover.visible=false
+reset=createSprite(300,120,100,10)
+reset.addImage(resetimg)
+reset.scale=0.5
+reset.visible=false
 }
 
 
 function draw(){
   //definir a cor do plano de fundo 
   background("white");
+  
+
   if(gamestate==PLAY){
-    score=score+Math.round(frameCount/60)
-    grouchao.velocityX=-2
-    if(keyDown("space")&&trex.y>140){
-      trex.velocityY = -10;
-      
+    score+=Math.round(getFrameRate()/60)
+    if(score>0&&score%100==0){
+    chekponto.play()
     }
-    trex.velocityY = trex.velocityY + 0.5
+    grouchao.velocityX=-(2+score/100)
+    if(keyDown("space")&&trex.y>161 ){
+      trex.velocityY = -10;
+      pulo.play()
+    }
+    
     if(grouchao.x<0){
       grouchao.x=grouchao.width/2        
     }
+    if (trex.isTouching(cactos)) {
+      gamestate=END
+      morte.play()
+      //trex.velocityY=-12
+      //pulo.play()
+    }
     shauwnuvens()
   spawnObstacles()
-  if (trex.isTouching(cactos)) {
-    gamestate=END
-  }
-  } else{
+  
+}
+
+      if (gamestate==END) {
+
+trex.changeAnimation("collide",trexcollide) 
   grouchao.velocityX=0
   cactos.setVelocityXEach(0)
   nuvens.setVelocityXEach(0)
+  cactos.setLifetimeEach(-1)
+  nuvens.setLifetimeEach(-1)
+  gameover.visible=true
+  reset.visible=true
   }
   text("Pontuação:"+ score ,500,50);
   
-  
+  trex.velocityY = trex.velocityY + 0.5
   //registrando a posição y do trex
   
   
@@ -78,13 +122,16 @@ function draw(){
   
  //impedir que o trex caia
   trex.collide(chaonaovisivel)
-  
+  if(mousePressedOver(reset)){
+    comecarDoZero()
+
+  }
   drawSprites();
 }
   function spawnObstacles(){
   if (frameCount % 60 === 0) {
-    var obstacle = createSprite(400,165,10,40);
-    obstacle.velocityX=-6
+    var obstacle = createSprite(600,165,10,40);
+    obstacle.velocityX=-(6+score/100)
 
     var rand = Math.round(random(1,6));
     switch(rand){
@@ -119,4 +166,14 @@ trex.depth = trex.depth + 1;
 nuvens.add(nuvem)
   }
 
+}
+function comecarDoZero(){
+  gamestate=PLAY
+  score=0
+  trex.changeAnimation("running", trex_running)
+  cactos.destroyEach()
+  nuvens.destroyEach()
+  reset.visible=false
+  gameover.visible=false
+  
 }
